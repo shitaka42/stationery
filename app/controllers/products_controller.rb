e493c38b2ecc7224c7a0d3a1fca3b428b2972bbf
tech-camp <tech-camp@tech-camp-047-no-MacBook-Air.local>
2016-01-24 19:44:55 +0900

    トップページ・個別ページの大枠のみ作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,8 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.order('id ASC').limit(20)
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
