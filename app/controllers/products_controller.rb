d6d472704de02cb7116d85cae5033e2d1d8b9802
tech-camp <tech-camp@tech-camp-047-no-MacBook-Air.local>
2016-01-25 11:49:35 +0900

    検索機能作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,10 @@ class ProductsController < ApplicationController
     @products = Product.all.order('id ASC').limit(20)
   end
 
+  def search
+    @products=Product.where('title LIKE(?)',"%#{params[:keyword]}%").limit(20)
+  end
+
   def show
     @product = Product.find(params[:id])
   end
