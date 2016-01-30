10b91ff6ee50830480749ca6ea04621811e01edf
tech-camp <tech-camp@tech-camp-047-no-MacBook-Air.local>
2016-01-30 12:21:55 +0900

    タグ機能ボタン　ビュー

--- app/controllers/genre_likes_controller.rb
+++ app/controllers/genre_likes_controller.rb
@@ -1,6 +1,7 @@
 class GenreLikesController < ApplicationController
   def genre_like
     product = Product.find(params[:product_id])
+    # @like_total=Genre_likes.where(genre_type: 1).group(product_id).count(genre_type)
     genre_like = current_user.genre_likes.build(product_id: product.id, genre_type:params[:genre_type])
     genre_like.save
     redirect_to controller: :products, action: :show, id: product.id
